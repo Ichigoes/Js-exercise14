2b19e4a3edc7289dd7b36762ab153ee6a4b8bd84
Ichigoes <102054584+Ichigoes@users.noreply.github.com>
2022-10-12 23:13:27 +0300

    Add files via upload

---
+++ Js Exercise14.js
@@ -0,0 +1,12 @@
+function numDef(input) {
+    let number = Number(input[0])
+    if (number < 100){
+        console.log("Less than 100")
+    } else if (100 <= number & number <= 200) {
+        console.log("Between 100 and 200")
+    } else {
+        console.log("Greater than 200")
+    }
+}
+
+numDef(["210"])
